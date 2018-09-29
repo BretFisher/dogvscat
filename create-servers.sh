10b94b1b71d83d8d7eb6dc0f9bc44f0d491fd709
Bret Fisher <bret@bretfisher.com>
2018-09-29 00:31:02 -0400

    various streamlining

--- create-servers.sh
+++ create-servers.sh
@@ -3,16 +3,22 @@
 
 # create managers servers in digital ocean with pre-set environment vars
 # https://docs.docker.com/machine/drivers/digital-ocean/
+
+# DO_TOKEN get the token from digitalocean.com (read/write)
+# DO_SIZE pick your droplet size from "doctl compute size list"
+# SSH_FINGERPRINT in the format of "8d:30:8a..." with a comand like "ssh-keygen -E md5 -lf  ~/.ssh/id_rsa.pub"
+
 for server in {1..3}; do
 docker-machine create \
   --driver=digitalocean \
   --digitalocean-access-token="${DO_TOKEN}" \
   --digitalocean-size="${DO_SIZE}" \
-  --digitalocean-private-networking=true \
   --digitalocean-ssh-key-fingerprint="${SSH_FINGERPRINT}" \
   --digitalocean-tags=dogvscat \
+  --digitalocean-private-networking=true \
   dvc${server} &
 done
+
 
 # if you wanted to create these locally in virtualbox, you might do this
 # remember to check if you have enough RAM
--- create-swarm.sh
+++ create-swarm.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 set -x
 
+# since we created droplets with a private NIC on eth1, lets use that for swarm comms
+LEADER_IP=$(docker-machine ssh dvc1 ifconfig eth1 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+
 # create a swarm as all managers
-docker-machine ssh dvc1 docker swarm init --listen-addr=eth1 --data-path-addr=eth1 --advertise-addr=eth1
+docker-machine ssh dvc1 docker swarm init --advertise-addr "$LEADER_IP"
 
-docker-machine ssh dvc1 docker swarm join-token manager
+# note that if you use eth1 above (private network in digitalocean) it makes the below
+# a bit tricky, because docker-machine lists the public IP's but we need the 
+# private IP of manager for join commands, so we can't simply envvar the token
+# like lots of scripts do... we'd need to fist get private IP of first node
 
-# copy this command and add run it on other nodes
-# docker-machine ssh dvc2 <paste command>
+# TODO: provide flexable numbers at cli for x managers and x workers
+JOIN_TOKEN=$(docker-machine ssh dvc1 docker swarm join-token -q manager)
+
+for i in 2 3; do
+  docker-machine ssh dvc$i docker swarm join --token "$JOIN_TOKEN" "$LEADER_IP":2377
+done
+
+docker-machine env dvc1
+
